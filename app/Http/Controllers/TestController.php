328da2554947b2c80b26bbb6e702487a09f2f085
ZacharyJia <jia199474@gmail.com>
2016-12-11 21:00:41 +0800

    feat: add a test api interface

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\VerifyEmail;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -19,7 +20,26 @@ class TestController extends Controller
 //        return view('welcome');
 //        Mail::to('jia199474@gmail.com')
 //            ->queue(new VerifyEmail());
-        dd(route("verify", ['token' => "heihei123", 'email' => '13281110@bjtu.edu.cn']));
+//        dd(route("verify", ['token' => "heihei123", 'email' => '13281110@bjtu.edu.cn']));
         return view('welcome');
+    }
+
+    public function api_test(Request $request)
+    {
+        $data = $request->input('data');
+        return [
+            "status" => "200",
+            "data" => [
+                [
+                    "name" => "zachary",
+                    "age" => 22,
+                ],
+                [
+                    "name" => "jia199474",
+                    "age" => 25,
+                ]
+            ],
+            "your_input" => $data
+        ];
     }
 }
---
+++ doc/api_test.md
@@ -0,0 +1,20 @@
+##test api for KLiu1996
+
+### api_test
+url: /api/test  
+params:
+```
+data: your input data, will be returned in the response, so that you can know wether your call is okay.
+```
+response:
+```
+{
+    "status":"200",
+    "data":
+    [
+        {"name":"zachary","age":22},
+        {"name":"jia199474","age":25}
+    ],
+    "your_input":"hahahah" //this is your original input data
+}
+```
