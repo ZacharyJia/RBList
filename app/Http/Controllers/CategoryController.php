8d4f14731d2371cc907a16a1fa488dadab7a04de
ZacharyJia <jia199474@gmail.com>
2017-01-22 15:01:10 +0800

    FEAT: 封装hashid方法，每次只加解密一个id

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,6 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Category;
-use Hashids;
 
 class CategoryController extends Controller
 {
@@ -22,7 +21,7 @@ class CategoryController extends Controller
         $data = $category->map(function($item, $key) {
             return [
                 "category_list" => [
-                    'id' => Hashids::encode($item['id']),
+                    'id' => hashid_encode($item['id']),
                     'name' => $item['name'],
                 ]
             ];
