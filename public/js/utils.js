3d13d2596dd8504378aa47a98a28c80d64e1f7ae
Yilin Lv <Yilin Lv>
2017-03-02 20:18:35 +0800

    FIX: email information address

--- public/js/utils.js
+++ public/js/utils.js
@@ -36,7 +36,7 @@ function checkLoginStatus(callback) {
     success: function (userinfo){
       if (userinfo.code === "200") {
         if (userinfo.data.verified === 0) {
-          verified_text = '您还没有验证邮箱 ' + '<a href=\'http://mail.bjtu.edu.cn/\' class=\'text-muted\'>去验证</a>';
+          verified_text = '您还没有验证邮箱 ' + '<a href=\'/need_verify\' class=\'text-muted\'>去验证</a>';
           new $.zui.Messager(verified_text, {
             type: 'info',
             icon: 'warning-sign',
